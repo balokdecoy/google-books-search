c2aac2556b63a54354cc781f2ac2fe037d564d34
johnniesimpson <johnniesimpson@outlook.com>
2021-04-16 19:59:47 -0400

    Update server index to link up to Mongo Atlas database.

--- server/index.js
+++ server/index.js
@@ -6,5 +6,16 @@ const app = express();
 app.use(express.json());
 
 app.listen(6000, () => {
-    console.log("Server running...");
+    console.log("Server connected...");
+    mongoose.connect(
+        "mongodb+srv://balokdecoy:1234@cluster0.pesnt.mongodb.net/googlebooks?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then(() => {
+        console.log("Connected to database...")
+    }).catch(error => {
+        console.log(error);
+    })
 })
